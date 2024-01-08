0a46179e32a631d2baad53368cacbca3d7041dbe
Abheetha Ishan Rangana <d.m.abheethaishan@gmail.com>
2024-01-08 16:47:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,25 +4,25 @@
 #  monitor_dimension = "SERVICE"
 #}
 
-resource "aws_ce_anomaly_subscription" "realtime_subscription" {
-  name      = "RealtimeAnomalySubscription"
-  frequency = "IMMEDIATE"
+#resource "aws_ce_anomaly_subscription" "realtime_subscription" {
+#  name      = "RealtimeAnomalySubscription"
+#  frequency = "IMMEDIATE"
 
-  monitor_arn_list = [
-    aws_ce_anomaly_monitor.anomaly_monitor.arn,
-  ]
+#  monitor_arn_list = [
+#    aws_ce_anomaly_monitor.anomaly_monitor.arn,
+#  ]
 
-  subscriber {
-    type    = "EMAIL"
-    address = "d.m.abheethaishan@gmail.com"
-  }
+#  subscriber {
+#    type    = "EMAIL"
+#    address = "d.m.abheethaishan@gmail.com"
+#  }
 
-  threshold_expression {
-    dimension {
-      key           = "ANOMALY_TOTAL_IMPACT_ABSOLUTE"
-      values        = ["100.0"]
-      match_options = ["GREATER_THAN_OR_EQUAL"]
-    }
+#  threshold_expression {
+#    dimension {
+#      key           = "ANOMALY_TOTAL_IMPACT_ABSOLUTE"
+#      values        = ["100.0"]
+#      match_options = ["GREATER_THAN_OR_EQUAL"]
+#    }
   }
 
 }
